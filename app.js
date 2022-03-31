8f8b6a0d0090659d39766722986deae79dd4f6b6
obaedulislam <obaed.cse@gmail.com>
2022-03-31 23:33:55 +0600

    basic calculation done

--- app.js
+++ app.js
@@ -12,7 +12,31 @@ function updateProductNumber(product, isIncreasing, price){
     const productTotal = document.getElementById(product + "-total");
     productTotal.innerText = productNumber * price;
 
+    // Calculate Total
+    calculateTotal();
 }
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + "-number");
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = (subTotal*10) / 100;
+    const totalPrice = subTotal + tax;
+
+    //Update on the HTML
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText =tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+
+
 //Handle Phone increase & decrease events
 document.getElementById("phone-plus").addEventListener("click", function(){
     updateProductNumber('phone', true, 1219);
