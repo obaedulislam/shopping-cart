b18892ec48b6f7a97a87a9c2cc2d8a4215b83ef7
obaedulislam <obaed.cse@gmail.com>
2022-03-31 19:51:20 +0600

    Case item Number Added

---
+++ app.js
@@ -0,0 +1,19 @@
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById("case-number");
+    const caseNumber = parseInt(caseInput.value);
+    if(isIncreasing == true){
+        caseInput.value = caseNumber + 1;
+    }
+    else(
+        caseInput.value = caseNumber - 1;
+    )
+}
+
+
+
+document.getElementById("case-plus").addEventListener("click", function(){
+    updateCaseNumber(true);
+});
+document.getElementById("case-minus").addEventListener("click", function(){
+    updateCaseNumber(false);
+})
